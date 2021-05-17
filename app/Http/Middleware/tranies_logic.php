01a2b71e5d5ba4e7e06e9ca48c4d45f2323b719f
Tran Nhi Khang <trannhikhang964587@gmail.com>
2021-05-17 08:26:21 +0700

    Initial commit

---
+++ app/Http/Middleware/tranies_logic.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class tranies_logic
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $per_page = $request->input('per_page');
+
+
+        //is null
+        if (empty($per_page)) {
+            $per_page = 10;
+        }
+
+        //is string
+        if (!is_numeric($per_page)) {
+            return redirect('error');
+        }
+
+       
+        if ($per_page <= 10) {
+            $per_page = 10;
+        }
+        else if($per_page > 10 && $per_page <= 30){
+            $per_page = 30;
+        }
+        else if ($per_page > 30 && $per_page <= 50){
+            $per_page = 50;
+        }
+        else if ($per_page >= 50){
+            $per_page = 50;
+        }
+        else {
+            $per_page = 50;
+        }
+        // $numbers = [10, 20, 30, 50];
+        // if(in_array($per_page, $numbers)){
+        //     $per_page = 10;
+        // }
+        $request->merge([ 'per_page' => $per_page]);
+
+        return $next($request);
+    }
+}
+
